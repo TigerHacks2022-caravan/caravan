67814dffd7121e1b07fdefdbaa98ece2b171838d
Luke Manary <69057730+xvyrq@users.noreply.github.com>
2022-11-06 01:21:21 -0500

    added caravan fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { CssBaseline, Grid } from "@material-ui/core";
-import { RecoilRoot } from "recoil";
-import { useRecoilState } from "recoil";
-import GlobalStyle from "./globalstyles";
-import Header from "./components/header/header";
-import List from "./components/list/list";
-import Map from "./components/map/map";
-import { caravanAtom } from "./atoms/atoms";
+import React, { useState, useEffect } from 'react'
+import { CssBaseline, Grid } from '@material-ui/core'
+import  { RecoilRoot }  from 'recoil';
+import { useRecoilState } from 'recoil';
+import GlobalStyle from './globalstyles';
+import Header from './components/header/header'
+import List from './components/list/list'
+import Map from './components/map/map'
+
 
 const App = () => {
-  const [coords, setCoords] = useState({});
-  const [bounds, setBounds] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [places, setPlaces] = useState([]);
+	const [coords, setCoords] = useState({})
+	const [bounds, setBounds] = useState(null)
+	const [places, setPlaces] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
 
-  const [caravans, setCaravans] = useRecoilState(caravanAtom);
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			({ coords: { latitude, longitude } }) => {
+				setCoords({ lat: latitude, lng: longitude })
+				console.log(latitude)
+				console.log(longitude)
+			}
+		)
+	}, [])
+	useEffect(() => {
+		fetchCaravans(
+			({ 
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        setCoords({ lat: latitude, lng: longitude });
-        console.log(latitude);
-        console.log(longitude);
-      }
-    );
-  }, []);
-  useEffect(() => {
-    const fetchCaravans = async () => {
-      const url = "http://localhost:5000/twee";
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log({ data, body: data.body });
-        // setCaravans(data.data)
-        console.log({ caravans });
-        // Set recoil
-      } catch (error) {
-        console.error(error);
-      }
-    };
+			})
+		)
+	}, [])
 
-    fetchCaravans();
-  }, []);
+	return (
+		<RecoilRoot>
+			
+			<GlobalStyle />
+			<CssBaseline />
+			<Header />
+			<Grid container spacing={3} style={{ width: '100%', backgroundColor: '#2d597b' }}>
+				<Grid item xs={12} md={4}>
+					<List isLoading={isLoading} />
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Map
+						setBounds={setBounds}
+						setCoords={setCoords}
+						coords={coords}
+						places={places}
+					/>
+				</Grid>
+			</Grid>
+		</RecoilRoot>
+	)
+}
 
-  return (
-    <RecoilRoot>
-      <GlobalStyle />
-      <CssBaseline />
-      <Header />
-      <Grid
-        container
-        spacing={3}
-        style={{ width: "100%", backgroundColor: "#2d597b" }}
-      >
-        <Grid item xs={12} md={4}>
-          <List isLoading={isLoading} />
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Map
-            setBounds={setBounds}
-            setCoords={setCoords}
-            coords={coords}
-            places={places}
-          />
-        </Grid>
-      </Grid>
-    </RecoilRoot>
-  );
-};
-
-export default App;
+export default App
