ba407b9c7e4493228eab116d173c5ad61f88ddca
ericstran <ericstran.stl@gmail.com>
2022-11-06 01:07:23 -0500

    some stuff

--- server/server.js
+++ server/server.js
@@ -1,39 +1,28 @@
 const express = require('express')
-// import express from 'express'
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())
 require('dotenv').config()
-// import dotenv from 'dotenv'
-// dotenv.config()
 const port = process.env.PORT || 5000
 
-const tweeHandler = require('./controllers/tweeHandler')
-// import tweeHandler from 'controllers/tweeHandler'
-// const tweeIdHandler = require('./controllers/tweeIdHandler')
-// import tweeIdHandler from 'controllers/tweeIdHandler'
+const tweeHandler = require('./controllers/tweeHandlers/tweeHandler')
+const tweeIdHandler = require('./controllers/tweeHandlers/tweeIdHandler')
 
-// // Connection URL
-// const url = process.env.MONGODB_URI
-// const client = new MongoClient(url)
-// // Database Name
-// const dbName = 'non-prod'
-
-// // Use connect method to connect to the server
-// await client.connect()
-// console.log('Connected successfully to server')
-// const db = client.db(dbName)
-// const collection = db.collection('caravans')
+const getUserHandler = require('./controllers/userHandlers/getUserIdHandler')
+const postUserHandler = require('./controllers/userHandlers/postUserHandler')
 
 // routes
 app.get('/', (req, res) => {
 	res.send('Hello World')
 })
 
-// app.get('/twee', (req, res) => {
-// 	return tweeHandler(req, res, collection)
-// })
+// twee (caravan) routes
 app.get('/twee', tweeHandler)
+app.get('/twee/:id', tweeIdHandler)
 
-// app.get('/twee/:id', tweeIdHandler)
+// user routes
+app.get('/user/:id', getUserHandler)
+app.post('/user', postUserHandler)
 
 // listener
 app.listen(port)
