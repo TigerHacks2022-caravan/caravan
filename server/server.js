bd946ca36e0a47053cb4d5c22e746167dce71e34
Luke Manary <69057730+xvyrq@users.noreply.github.com>
2022-11-06 02:06:36 -0600

    cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 app.use(bodyParser.json())
+app.use(cors())
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
